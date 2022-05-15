f82a0f6b60e103a129e758f67be29854b26df251
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-15 21:54:46 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -122,12 +122,12 @@ async def start(client: Client, message: Message):
     if len(message.command) != 2:
         reply_markup = InlineKeyboardMarkup(butonlar)
         return await message.reply_photo(
-            photo=PICS
+            photo=PICS,
             caption=START_TXT.format(
             message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME),     
             reply_markup=reply_markup, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-    except Exception as e:
-        await message.reply_text(str(e)) 
+        except Exception as e:
+            await message.reply_text(str(e)) 
     file_id = message.command[1]
     files_ = await get_file_details(file_id)
     if not files_:
