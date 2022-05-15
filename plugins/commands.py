8c2d4038f1dc40ee54fbb0545efcb00e9bffb0c2
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-15 19:19:55 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -64,7 +64,7 @@ async def start(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(butonlar)
         await client.send_photo(
             chat_id=message.from_user.id,
-            photo=random.choice(PICS),
+            photo=PICS,
             caption=START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup, 
             disable_web_page_preview=True,
@@ -125,7 +125,7 @@ async def start(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(butonlar)
         return await client.send_photo(
                    chat_id=message.from_user.id,
-                   photo=random.choice(PICS),
+                   photo=PICS,
                    caption=START_TXT.format(message.from_user.mention),
                    reply_markup=reply_markup, 
                    parse_mode=ParseMode.HTML, 
