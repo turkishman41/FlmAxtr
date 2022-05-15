2bc2187109674e2fe9b4a99a06e66231061734ae
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-15 18:57:20 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -127,7 +127,10 @@ async def start(client: Client, message: Message):
             photo=PICS,
             caption=START_TXT.format(
             message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup, parse_mode=ParseMode.HTML, disable_web_page_preview=True)  
+            reply_markup=reply_markup, 
+            parse_mode=ParseMode.HTML, 
+            disable_web_page_preview=True
+               )  
     file_id = message.command[1]
     files_ = await get_file_details(file_id)
     if not files_:
